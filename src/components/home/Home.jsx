25207454fed815f4f9d3c4e6fdb9d5bfe681b163
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-08-17 12:51:37 -0500

    Home Layout

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+/* import { useState } from 'react' */
+import "../../assets/style/Home.css";
+
+function Home() {
+  /* const [count, setCount] = useState(0) */
+
+  return (
+    <>
+      <div className="homeCont">
+        <h1>Test</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
