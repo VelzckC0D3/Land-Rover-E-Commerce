0f80f93c99e6c31fff230b83b6ac08303c172160
Daniel Falcon <Danfall963@gmail.com>
2023-08-21 01:37:58 -0400

    update Navabar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,11 @@ const Navbar = () => (
   <div>
     <ul>
       <li>
+        <Link to="/">
+          HOME
+        </Link>
+      </li>
+      <li>
         <Link to="/sign_up">
           Sign Up
         </Link>
