c62412b2fa44fc032d57d0e1447c0dd6bba01fc6
Daniel Falcon <Danfall963@gmail.com>
2023-08-21 01:32:23 -0400

    redux main files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-/* import { useState } from 'react' */
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import './index.css';
+import Home from './pages/Home';
+import Registration from './components/auth/Registration';
+import Login from './components/auth/Login';
+import Logout from './components/auth/Logout';
 
-function App() {
-  /* const [count, setCount] = useState(0) */
+import Navbar from './components/Navbar';
 
-  return (
-    <>
-      <h1>Test</h1>
-    </>
-  );
-}
+const App = () => (
+  <React.StrictMode>
+    <Router>
+      <Toaster />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign_up" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
 
 export default App;
