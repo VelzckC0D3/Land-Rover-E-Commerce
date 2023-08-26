0ece8c0bdd19b6d61360820638ecc5c2e2c00816
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-08-26 18:52:26 -0500

    Set up Nav menu
    - created an interactive lateral menu
    - set up home page

--- src/components/car/car_details.jsx
+++ src/components/car/car_details.jsx
@@ -1,7 +1,7 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import '../../assets/style/Home.css';
+import '../../assets/style/Vehicles.css';
 
 function CarDetails() {
   const { carId } = useParams();
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
+import '../assets/style/Home.css'
+import { useNavigate } from 'react-router-dom';
+import { AiOutlineRightCircle } from 'react-icons/ai';
+
 const Home = () => {
+  const navigate = useNavigate();
   return (
-    <div className="container">
-      <h2>Home</h2>
+    <>
+
+    <div className="homeCont">
+      <h2 className='homeTitle'>LAND ROVER</h2>
+      <button className='actionBtn' onClick={() => navigate('/vehicles')}>Learn More <AiOutlineRightCircle /> </button>
     </div>
+    </>
   )
 }
 
