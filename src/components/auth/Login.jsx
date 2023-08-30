16f13fbb868aacbcbe2fddd3c69a32751e7f486f
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-08-30 18:37:35 +0500

    Update Login page to make its UI/UX attractive

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,37 +1,52 @@
-import { useDispatch } from 'react-redux';
-import { useForm } from 'react-hook-form';
-import { loginUser } from '../../features/auth/authActions';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import { loginUser } from "../../features/auth/authActions";
+import { useNavigate } from "react-router-dom";
+import "../../assets/style/Login.css";
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (formData) => {
     dispatch(loginUser(formData));
-    navigate('/')
+    navigate("/");
   };
 
   return (
-    <div className='container'>
+    <div className="container">
       <h2>Login</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="email"
-          name="email"
-          placeholder="E-mail"
-          {...register('email', { required: true })}
-        />
-        {errors.email && <span>Email is required</span>}
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            {...register("email", { required: "Email is required" })}
+          />
+          {errors.email && (
+            <span className="error">{errors.email.message}</span>
+          )}
+        </div>
 
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          {...register('password', { required: true })}
-        />
-        {errors.password && <span>Password is required</span>}
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            {...register("password", { required: "Password is required" })}
+          />
+          {errors.password && (
+            <span className="error">{errors.password.message}</span>
+          )}
+        </div>
 
         <button type="submit">Login</button>
       </form>
