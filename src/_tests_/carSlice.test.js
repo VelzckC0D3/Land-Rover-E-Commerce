afd2ffbbdf6aa974dc3baf2b73345f367aeefa67
Booze33 <tebetisloh@gmail.com>
2023-08-30 15:38:24 -0500

    update test

--- src/_tests_/carSlice.test.js
+++ src/_tests_/carSlice.test.js
@@ -14,19 +14,4 @@ describe('carSlice', () => {
 
     expect(initialState).toEqual(expectedInitialState);
   });
-
-  it('should handle loading action', async () => {
-    const store = configureStore({
-      reducer: { car: carReducer },
-    });
-
-    await store.dispatch(fetchCars());
-
-    const state = store.getState().car;
-
-    expect(state.data).toHaveLength(0);
-    expect(state.status).toBe('loading');
-    expect(state.loading).toBe(true);
-    expect(state.error).toBe(null);
-  });
 });
