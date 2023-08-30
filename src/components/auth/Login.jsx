b63f7a6c01412bf6e97bfb39b80772333c855645
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-08-30 18:46:38 +0500

    Add section tag to wrap the complete Login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -19,38 +19,40 @@ const Login = () => {
   };
 
   return (
-    <div className="container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-group">
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter your email"
-            {...register("email", { required: "Email is required" })}
-          />
-          {errors.email && (
-            <span className="error">{errors.email.message}</span>
-          )}
-        </div>
+    <section className="loginContainer">
+      <div className="container">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              {...register("email", { required: "Email is required" })}
+            />
+            {errors.email && (
+              <span className="error">{errors.email.message}</span>
+            )}
+          </div>
 
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            {...register("password", { required: "Password is required" })}
-          />
-          {errors.password && (
-            <span className="error">{errors.password.message}</span>
-          )}
-        </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              {...register("password", { required: "Password is required" })}
+            />
+            {errors.password && (
+              <span className="error">{errors.password.message}</span>
+            )}
+          </div>
 
-        <button type="submit">Login</button>
-      </form>
-    </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </section>
   );
 };
 
