f96a4c18e103d646c7f34f68a6a79931ae7f1b7f
emmanuel mhango <emmanuellmhango@gmail.com>
2023-08-30 16:53:00 +0200

    Updated api url calling. put the api urls in one file for easy management

--- src/features/auth/authActions.js
+++ src/features/auth/authActions.js
@@ -1,82 +1,91 @@
-import axios from 'axios';
-import { authRequest, authSuccess, authFailure, logout } from './authSlice';
-import { toast } from 'react-hot-toast';
-
-const API_BASE_URL = 'http://192.168.1.1:3000';
+import axios from "axios";
+import { authRequest, authSuccess, authFailure, logout } from "./authSlice";
+import { toast } from "react-hot-toast";
+import apiURL from "../auth/urls";
 
 export const registerUser = (formData) => async (dispatch) => {
-    dispatch(authRequest());
+  dispatch(authRequest());
 
-    try {
-        const response = await axios.post(`${API_BASE_URL}/signup`, { user: formData }, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+  try {
+    const response = await axios.post(
+      `${apiURL}/signup`,
+      { user: formData },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-        if (response.status === 200) {
-            const token = response.headers.authorization;
-            const user = response.data.data;
-            dispatch(authSuccess({ token, user }));
-            localStorage.setItem('token', token);
-            localStorage.setItem('user', JSON.stringify(user));
-            toast.success(`Welcome, ${user.name}`);
-        } else {
-            throw new Error(response.statusText);
-        }
-    } catch (error) {
-        dispatch(authFailure(error.message));
-        toast.error('Registration failed. Please check your information and try again.');
+    if (response.status === 200) {
+      const token = response.headers.authorization;
+      const user = response.data.data;
+      dispatch(authSuccess({ token, user }));
+      localStorage.setItem("token", token);
+      localStorage.setItem("user", JSON.stringify(user));
+      toast.success(`Welcome, ${user.name}`);
+    } else {
+      throw new Error(response.statusText);
     }
+  } catch (error) {
+    dispatch(authFailure(error.message));
+    toast.error(
+      "Registration failed. Please check your information and try again."
+    );
+  }
 };
 
 export const loginUser = (formData) => async (dispatch) => {
-    dispatch(authRequest());
+  dispatch(authRequest());
 
-    try {
-        const response = await axios.post(`${API_BASE_URL}/login`, { user: formData }, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+  try {
+    const response = await axios.post(
+      `${apiURL}/login`,
+      { user: formData },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-        if (response.status === 200) {
-            const token = response.headers.authorization;
-            const user = response.data.data;
-            dispatch(authSuccess({ token, user }));
-            localStorage.setItem('token', token);
-            localStorage.setItem('user', JSON.stringify(user));
-            toast.success(`Welcome, ${user.name}`);
-        } else {
-            const errorResponse = response.data || 'An error occurred.';
-            throw new Error(errorResponse);
-        }
-    } catch (error) {
-        dispatch(authFailure(error.message));
-        toast.error(`Login failed. ${error.message}`);
+    if (response.status === 200) {
+      const token = response.headers.authorization;
+      const user = response.data.data;
+      dispatch(authSuccess({ token, user }));
+      localStorage.setItem("token", token);
+      localStorage.setItem("user", JSON.stringify(user));
+      toast.success(`Welcome, ${user.name}`);
+    } else {
+      const errorResponse = response.data || "An error occurred.";
+      throw new Error(errorResponse);
     }
+  } catch (error) {
+    dispatch(authFailure(error.message));
+    toast.error(`Login failed. ${error.message}`);
+  }
 };
 
 export const logoutUser = () => async (dispatch, getState) => {
-    try {
-        const { token } = getState().auth;
-        const response = await axios.delete(`${API_BASE_URL}/logout`, {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: token,
-            },
-        });
+  try {
+    const { token } = getState().auth;
+    const response = await axios.delete(`${apiURL}/logout`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    });
 
-        if (response.status === 200) {
-            dispatch(logout());
-            localStorage.removeItem('token');
-            localStorage.removeItem('user');
-            toast.success('Logged out successfully.');
-        } else {
-            throw new Error(response.statusText);
-        }
-    } catch (error) {
-        dispatch(authFailure(error.message));
-        toast.error('Logout failed. Please try again.');
+    if (response.status === 200) {
+      dispatch(logout());
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      toast.success("Logged out successfully.");
+    } else {
+      throw new Error(response.statusText);
     }
+  } catch (error) {
+    dispatch(authFailure(error.message));
+    toast.error("Logout failed. Please try again.");
+  }
 };
---
+++ src/features/auth/urls.js
@@ -0,0 +1,3 @@
+const apiURL = "http://192.168.56.1:5173";
+
+export default apiURL;
