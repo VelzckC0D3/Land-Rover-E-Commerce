ced14de1cffa5ebf947eb1f8cc08525407670cfa
emmanuel mhango <emmanuellmhango@gmail.com>
2023-08-31 12:33:43 +0200

    Update carSlice.js

--- src/features/cars/carSlice.js
+++ src/features/cars/carSlice.js
@@ -1,7 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import apiURL from "../auth/urls";
+//import apiURL from "../auth/urls";
 
+const apiURL = 'http://192.168.1.1:3000';
 // Thunks
 export const fetchCars = createAsyncThunk("car/fetchCars", async () => {
   const response = await axios.get(`${apiURL}/api/v1/cars/`);
