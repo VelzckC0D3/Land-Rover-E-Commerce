b710b454f2429403f34d43776991aeb75bf73fab
emmanuel mhango <emmanuellmhango@gmail.com>
2023-08-31 12:08:28 +0200

    Modified reservSlice.js, added apiURL as a file holding a call to the api

--- src/features/reservation/reservSlice.js
+++ src/features/reservation/reservSlice.js
@@ -1,8 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-// import apiURL from '../auth/url';
-// API URL
-const apiURL = 'http://192.168.1.1:3000';
+import apiURL from '../auth/url';
 
 // Thunks
 export const fetchReservations = createAsyncThunk('reservation/fetchReservations', async () => {
